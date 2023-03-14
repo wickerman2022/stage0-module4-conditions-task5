463ee0c924068d584c413d9a43bd78b9110dd768
wickerman2019 <sverige@i.ua>
2023-03-14 11:04:26 +0200

    Fix bug with default incorrect value

--- src/main/java/school/mjc/stage0/conditions/task5/SwitchDaysInMonthPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/SwitchDaysInMonthPrinter.java
@@ -13,6 +13,7 @@ public class SwitchDaysInMonthPrinter {
                 System.out.println(30);
                 break;       
             default:
+                System.out.println("wrong number!");
                 break;
         }
     }
